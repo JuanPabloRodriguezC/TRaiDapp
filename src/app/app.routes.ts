e45d14ccdfd63919a8905d1a85fd58f19c886ff7
JuanPabloRodriguezC <jprodriguezcano99@gmail.com>
2025-04-03 10:02:54 -0600

    add mat-container

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,10 +4,9 @@ import { MainpageComponent } from './client/pages/mainpage/mainpage.component';
 import { SettingsComponent } from './client/pages/settings/settings.component';
 import { DashboardComponent } from './client/pages/dashboard/dashboard.component';
 import { ExploreBotComponent } from './client/pages/explore-bot/explore-bot.component';
-import { MaterialSampleComponent } from './client/material-reference';
 
 export const routes: Routes = [
-    {path: "", component:MaterialSampleComponent},
+    {path: "", component:MainpageComponent},
     {path: "browse", component:BrowseComponent},
     {path: "settings", component:SettingsComponent},
     {path: "dashboard", component:DashboardComponent},
