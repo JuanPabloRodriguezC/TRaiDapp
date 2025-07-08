8ccee49c83511f90809702a04a3cca19bf5fdd7e
JuanPabloRodriguezC <jprodriguezcano99@gmail.com>
2025-07-07 19:46:43 -0600

    added temporary logos

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,6 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
-import Aura from '@primeng/themes/aura';
 import { providePrimeNG } from 'primeng/config';
 import { provideHttpClient } from '@angular/common/http';
 import MyPreset from './my_preset';
