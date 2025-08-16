afef509863f48e9e08b762eb4b30eecfaa7bb22e
JuanPabloRodriguezC <jprodriguezcano99@gmail.com>
2025-08-16 14:26:56 -0600

    sidebar layout update

--- src/app/Layout/components/menu/menu.component.ts
+++ src/app/Layout/components/menu/menu.component.ts
@@ -16,11 +16,45 @@ export class AppMenuComponent {
 
   clienteMenu: MenuItem[] = [
     {
+      label: 'TRaiDapp',
       items: [
         { label: 'Home', icon: 'pi pi-fw pi-home', routerLink: ['/main/landing-page'] },
         { label: 'Browse', icon: 'pi pi-fw pi-sliders-v', routerLink: ['/main/browse'] },
         { label: 'Dashboard', icon: 'pi pi-fw pi-mobile', class: 'rotated-icon', routerLink: ['/main/dashboard'] },
         
+      ]
+    },
+    {
+      label: 'Resources',
+      items: [
+        { label: 'Learn', icon: 'pi pi-fw pi-book',
+          items: [
+            {
+                label: 'Smart Contracts',
+                icon: 'pi pi-fw pi-file-check',
+                routerLink: ['/auth/login']
+            },
+            {
+                label: 'What are Agents?',
+                icon: 'pi pi-fw pi-microchip-ai',
+                routerLink: ['/auth/login']
+            },
+            {
+                label: 'Trading Strategies',
+                icon: 'pi pi-fw pi-chart-line',
+                routerLink: ['/auth/error']
+            },
+          ]
+        },
+        { label: 'Documentation', icon: 'pi pi-fw pi-book', routerLink:['']}
+      ]
+    },
+    {
+      label: 'Social',
+      items: [
+        { label: 'GitHub', icon: 'pi pi-fw pi-github', routerLink:['']},
+        { label: 'X', icon: 'pi pi-fw pi-twitter', routerLink:['']},
+        { label: 'Discord', icon: 'pi pi-fw pi-discord', routerLink:['']}
       ]
     }
   ];
